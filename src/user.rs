8c949558056d78ccf89dc9759c740a30934efc34
BennyYip <yebenmy@protonmail.com>
2017-05-20 02:59:27 +0800

    fix clipply warnning

--- src/user.rs
+++ src/user.rs
@@ -2,7 +2,6 @@ use chrono::prelude::*;
 use reqwest;
 use errors::*;
 use std::io::Read;
-use std::str::FromStr;
 use json::JsonValue;
 use json;
 
@@ -13,10 +12,9 @@ pub struct User {
 }
 
 lazy_static! {
-    static ref client: reqwest::Client =  reqwest::Client::new().expect("cannot create reqwest client");
-    static ref now:DateTime<UTC>= UTC::now();
+    static ref CLIENT: reqwest::Client =  reqwest::Client::new().expect("cannot create reqwest client");
+    static ref NOW:DateTime<UTC>= UTC::now();
 }
-static GITHUB_API: &'static str = "https://api.github.com/users/{}/events";
 
 impl User {
     pub fn new(username: &str, email: &str, timezone: i32, is_east: bool) -> Self {
@@ -28,12 +26,9 @@ impl User {
             timezone: FixedOffset::east(timezone),
         }
     }
-    fn is_my_commit(&self, commit: &JsonValue) -> bool {
-        commit["author"]["email"] == self.email
-    }
     fn get_events(&self) -> Result<JsonValue> {
         let mut j = String::new();
-        client
+        CLIENT
             .get(&format!("https://api.github.com/users/{}/events", self.username))
             .send()?
             .read_to_string(&mut j)?;
@@ -41,25 +36,22 @@ impl User {
     }
     pub fn today_has_push(&self) -> Result<bool> {
         let events = self.get_events()?;
-        let events = match events {
-            JsonValue::Array(arr) => {
-                arr.into_iter()
-                    .filter(|x| x["type"] == "PushEvent")
-                    .collect()
-            }
-            _ => vec![],
-        };
-        for x in events {
-            let push_date = x["created_at"]
-                .as_str()
-                .unwrap()
-                .parse::<DateTime<UTC>>()?
-                .with_timezone(&self.timezone);
-            if is_same_day(&push_date, &now.with_timezone(&self.timezone)) {
-                return Ok(true);
+        if let JsonValue::Array(events) = events {
+            for x in events.iter().filter(|x| x["type"] == "PushEvent") {
+                let push_date = x["created_at"]
+                    .as_str()
+                    .unwrap()
+                    .parse::<DateTime<UTC>>()?
+                    .with_timezone(&self.timezone);
+                if is_same_day(&push_date, &NOW.with_timezone(&self.timezone)) {
+                    return Ok(true);
+                }
             }
         }
         Ok(false)
+    }
+    fn is_my_commit(&self, commit: &JsonValue) -> bool {
+        commit["author"]["email"] == self.email
     }
 }
 
